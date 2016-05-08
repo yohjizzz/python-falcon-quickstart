a664517711fc8e36d923e0f47999f687ead69a24
Yohji Nihonyanagi <y_nihonyanagi@ardito.jp>
2016-05-09 01:09:47 +0900

    * Falcon v1.0.0rc2 対応 * ドキュメントストリング追加

--- extension.py
+++ extension.py
@@ -3,24 +3,35 @@
 class ExtensionComponent(object):
     """
     Falcon’s middleware
+    基本は Falcon で管理されるすべてのリソース (というかリクエスト) に対して一律で適用されます
     """
     def process_request(self, req, resp):
+        """
+        リクエスト受信直後にコールされます
+        URI に対するリソースが見つからなくても、このメソッドはコールされます
+        """
         print("middleware: process before routing")
 
-    def process_resource(self, req, resp, resource):
+    def process_resource(self, req, resp, resource, param):
+        """
+        URL に対するリソースの直前にコールされます
+        URI に対するリソースが見つからなければ、このメソッドはコールされません (v0.3.0 ではコールされてた)
+        引数 param は v1.0.0 で追加された (v0.3.0 では存在しない)
+        """
         print("middleware: process before resource", resource)
 
     def process_response(self, req, resp, resource):
+        """
+        レスポンス送信直前にコールされます
+        URL に対するリソースが見つからなくても、このメソッドはコールされます
+        """
         print("middleware: process before response", resource)
 
 
 def before_resource(req, resp, resource, params):
     """
     Falcon's hook function for before resources
-    :param req: Request
-    :param resp: Response
-    :param resource:
-    :param params:
+    @falcon.before(..) や @falcon.after(..) を使ってファンクションまたはリソースクラス単位で指定します
     """
     print("hook: process before resource", resource)
 
@@ -28,8 +39,6 @@ def before_resource(req, resp, resource, params):
 def after_resource(req, resp, resource):
     """
     Falcon's hook function for after resources
-    :param req:
-    :param resp:
-    :param resource:
+    @falcon.before(..) や @falcon.after(..) を使ってファンクションまたはリソースクラス単位で指定します
     """
     print("hook: process after resource", resource)
